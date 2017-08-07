390a366cad4547c5427834b4f7b2ba7ee36618f6
JYLim <junyaolim0206@gmail.com>
2017-08-07 22:26:29 +0800

    Create tasker.bat

---
+++ tasker.bat
@@ -0,0 +1,30 @@
+::raw code
+::-JYLim
+
+-@echo off		
+ -color a		
+ -title Tasker (JYLim#4802)		
+ 		
+ -:ANSWER		
+ -set /P c=Choose an action. (Shutdown/Restart/Hibernate/Cancel) [S/R/H/X]		
+ -if /I "%c%" EQU "S" goto :shutdown		
+ -if /I "%c%" EQU "R" goto :restart		
+ -if /I "%c%" EQU "H" goto :hibernate		
+ -if /I "%c%" EQU "X" goto :exit		
+ -goto :ANSWER		
+ -		
+ -:shutdown		
+ -c:\windows\system32\shutdown -s -f -t 02  		
+ -exit		
+ -		
+ -:restart		
+ -c:\windows\system32\shutdown -r -f -t 02		
+ -exit		
+ -		
+ -:hibernate		
+ -rundll32.exe powrprof.dll,SetSuspendState 0,1,0		
+ -exit		
+ -		
+ -:exit		
+ -echo Cancelled menu.		
+ -exit 
